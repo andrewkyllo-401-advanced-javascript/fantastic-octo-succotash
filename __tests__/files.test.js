2aa028d00bd64eb7e251d67e7e2bbc89b582ec04
Andrew Kyllo <kyllo34@gmail.com>
2020-03-02 13:57:38 -0800

    added tests for files and logger

---
+++ __tests__/files.test.js
@@ -0,0 +1,26 @@
+jest.mock('fs');
+const files = require('../lib/files');
+
+describe('files module', () => {
+  it('can load a file', () => {
+    return files.loadFile('foo.text')
+      .then(contents => {
+        expect(Buffer.isBuffer(contents)).toBeTruthy();
+      })
+  })
+  it('can save a file', () => {
+
+  })
+
+  it('raises an error if a file is invalid', () => {
+
+  })
+
+  it('can uppercase a buffer of text', () => {
+
+  })
+
+  it('can alter a file', () => {
+
+  })
+})
